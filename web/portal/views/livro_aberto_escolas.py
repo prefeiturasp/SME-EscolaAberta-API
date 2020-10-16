ea2b5f09f19201b894e37db6dddfcdaad9b25412
BrunoTSBuzzo <brunshzl@gmail.com>
2020-10-16 13:09:46 -0300

    criando query para ano atual e 2019

--- web/portal/views/livro_aberto_escolas.py
+++ web/portal/views/livro_aberto_escolas.py
@@ -70,7 +70,35 @@ class LivroAbertoModelViewSet(viewsets.GenericViewSet):
 
     @action(detail=False, url_path='ano-atual', methods=['GET'])
     def ano_atual(self, request):
-        return Response(data={'detail': 'resultados atuais'},
+        query = """
+        select 
+            escolas.*,
+            turmas.total_vagas,
+            coalesce(servidores.total_servidores, 0) as total_servidores,
+            coalesce(turmas.total_matriculados, 0) as total_matriculados
+        from escolas_escolas as escolas
+            inner join (select 
+                            codesc,
+                            sum(vagofer) total_vagas,
+                            sum(matric) as total_matriculados
+                        from turmas_turmas as turmas
+                        where cdserie notnull
+                            and escolarizacao = 'S'
+                            and matric notnull
+                        group by codesc) turmas
+                on turmas.codesc = escolas.codesc
+            left join (select   
+                            count(cd_serv_sme) as total_servidores,
+                            cd_unidade_educacao_atual
+                        from servidores_servidores ss
+                        group by cd_unidade_educacao_atual) servidores
+                on escolas.codesc = servidores.cd_unidade_educacao_atual
+        where tipoesc != 'ESC.PART.';
+        """
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return Response(data={'results': [dict(zip([column[0] for column in cursor.description], row))
+                                          for row in cursor.fetchall()]},
                         status=status.HTTP_200_OK)
 
     @action(detail=False, url_path='2018', methods=['GET'])
@@ -80,5 +108,33 @@ class LivroAbertoModelViewSet(viewsets.GenericViewSet):
 
     @action(detail=False, url_path='2019', methods=['GET'])
     def ano_de_2019(self, request):
-        return Response(data={'detail': 'resultados de 2019'},
+        query = """
+                select 
+                    escolas.*,
+                    turmas.total_vagas,
+                    coalesce(servidores.total_servidores, 0) as total_servidores,
+                    coalesce(turmas.total_matriculados, 0) as total_matriculados
+                from escolas_escolas_2019 as escolas
+                    inner join (select 
+                                    codesc,
+                                    sum(vagofer) total_vagas,
+                                    sum(matric) as total_matriculados
+                                from turmas_turmas_2019 as turmas
+                                where cdserie notnull
+                                    and escolarizacao = 'S'
+                                    and matric notnull
+                                group by codesc) turmas
+                        on turmas.codesc = escolas.codesc
+                    left join (select   
+                                    count(cd_serv_sme) as total_servidores,
+                                    cd_unidade_educacao_atual
+                                from servidores_servidores_2019 ss
+                                group by cd_unidade_educacao_atual) servidores
+                        on escolas.codesc = servidores.cd_unidade_educacao_atual
+                where tipoesc != 'ESC.PART.';
+                """
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return Response(data={'results': [dict(zip([column[0] for column in cursor.description], row))
+                                          for row in cursor.fetchall()]},
                         status=status.HTTP_200_OK)
